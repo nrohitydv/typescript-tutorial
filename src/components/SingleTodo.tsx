1fa096cde6dcaf8e5ed5024375abfce1c504c952
Rohit <rohityadav787898@gmail.com>
2024-05-02 15:21:24 +0545

    Able to mark up done

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import "./style.css";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import TodoList from "./TodoList";
 type Props = {
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
   return (
-    <form className="todo__single">
-      <span className="todo__single--text">{todo.todo}</span>
+    <form className="todos__single">
+      {todo.isDone ? (
+        <span className="todos__single--text">{todo.todo}</span>
+      ) : (
+        <s className="todos__single--text">{todo.todo}</s>
+      )}
       <div>
         <span className="icon">
           <AiFillEdit />
@@ -18,7 +31,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         <span className="icon">
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
